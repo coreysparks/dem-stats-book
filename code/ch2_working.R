2b18389797b90ae890b42a843d15fef16019045a
Corey Sparks <corey.sparks@utsa.edu>
2021-06-23 14:15:44 -0500

    changes 6/23

--- code/ch2_working.R
+++ code/ch2_working.R
@@ -24,9 +24,27 @@ ddi<- read_ipums_ddi(ddi_file = "data/usa_00096.xml")
 ipums <- read_ipums_micro(ddi = ddi)
 
 
-dhs<-haven::read_dta("data/zzir62dt/ZZIR62FL.DTA")
+dhs<-haven::read_dta("https://github.com/coreysparks/data/blob/master/ZZIR62FL.DTA?raw=true")
+dhs<-haven::zap_labels(dhs)
 dhs$v005<- dhs$v005/1000000
-dhsdes<-svydesign(ids = ~v021,
+dhs2<- select(dhs, v021, v022, v005, v139, v151)
+
+names(dhs2)
+library(dplyr); library(gtsummary)
+options(survey.lonely.psu = "adjust")
+t1<- survey::svydesign(ids = ~v021,
+               strata=~v022,
+               weights = ~v005,
+               data=dhs2)%>%
+  gtsummary::tbl_svysummary(by = "v139", include = c(v139,v151))%>%
+  as_kable()
+
+survey::svydesign(ids = ~v021,
                   strata=~v022,
                   weights = ~v005,
-                  data=dhs)
+                  data=dhs2)%>%
+  tbl_svysummary(by = "v139", include = c(v151, v139))
+
+
+library(tableone)
+svyCreateTableOne(vars = "v151", strata= "v139", data=t1)
